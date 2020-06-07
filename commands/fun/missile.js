73b3439b22c37d4017d31311bb5b32e9b7f63f65
peregrine-developments <peregrine.developments@gmail.com>
2020-06-06 18:19:55 -0700

    Lmao now it better

--- commands/fun/missile.js
+++ commands/fun/missile.js
@@ -2,7 +2,7 @@ const config = require("../../config/config.json");
 
 exports.run = (client, message, args) => {
     let designator = "missile";
-    if(args.length < 1) designator = args.join(" ");
+    if(args.length > 0) designator = args.join(" ");
     
     return message.channel.send("**The " + designator + " knows where it is at all times. It knows this because it knows where it isn't. By subtracting where it is from where it isn't, or where it isn't from where it is (whichever is greater), it obtains a difference, or deviation. The guidance subsystem uses deviations to generate corrective commands to drive the " + designator + " from a position where it is to a position where it isn't, and arriving at a position where it wasn't, it now is. Consequently, the position where it is, is now the position that it wasn't, and it follows that the position that it was, is now the position that it isn't." + "\n\n" +
                                 "In the event that the position that it is in is not the position that it wasn't, the system has acquired a variation, the variation being the difference between where the " + designator + " is, and where it wasn't. If variation is considered to be a significant factor, it too may be corrected by the GEA. However, the " + designator + " must also know where it was." + "\n\n" +
